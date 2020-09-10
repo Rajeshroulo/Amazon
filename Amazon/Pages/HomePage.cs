1aea20ac81d31b8a374ffc5971fd42481553c5a8
Rajeshroulo <rajraval017@gmail.com>
2020-09-10 16:13:31 +0530

    Added code to search mobile and place order in amazon

---
+++ Amazon/Pages/HomePage.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Amazon.Pages
+{
+   public class HomePage
+   {
+
+        public IWebDriver driver;
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//input[@id='twotabsearchtextbox']")]
+        public IWebElement searchBox;
+
+        [FindsBy(How = How.XPath, Using = "//header//div[2]//div[1]//input[1]")]
+        public IWebElement search;
+
+        [FindsBy(How = How.XPath, Using = "//span[contains(text(),'OnePlus 7 (Mirror Grey, 6GB RAM, Optic AMOLED Disp')]")]
+        public IWebElement onePlus;
+
+        [FindsBy(How = How.XPath, Using = "//input[@id='buy-now-button']")]
+        public IWebElement buyNow;
+
+        [FindsBy(How = How.XPath, Using = "//input[@id='ap_password']")]
+        public IWebElement passWord;
+
+        [FindsBy(How = How.Id, Using = "signInSubmit")]
+        public IWebElement login;
+
+
+        public void SearchMobile(string password)
+        {
+            searchBox.SendKeys(ConfigurationManager.AppSettings["Model"]);
+            search.Click();
+            Thread.Sleep(9000);
+            onePlus.Click();
+            List<string> windows = driver.WindowHandles.ToList();
+            foreach (var mobilewindow in windows)
+            {
+                driver.SwitchTo().Window(mobilewindow);
+            }
+            Thread.Sleep(5000);
+            buyNow.Click();
+            passWord.SendKeys(password);
+            login.Click();
+            Thread.Sleep(5000);
+        }
+   }
+}
--- Amazon/Test/TestClass.cs
+++ Amazon/Test/TestClass.cs
@@ -2,23 +2,25 @@
 using Amazon.Data;
 using Amazon.Pages;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Amazon.Test
 {
-   public class TestClass : BaseClass
-   {
+    public class TestClass : BaseClass
+    {
         UserData data = new UserData();
-       [Test]  
+      [Test,Order(1)]  
        public void AccountLogin()
        {
             var login = new LoginPage(driver);
             login.AmazonLogin(data.email,data.password);
             Assert.AreEqual("Mobiles", login.MobilePhones());
+       } 
+
+       [Test,Order(2)]
+       public void MobileSearch()
+       {
+            var search = new HomePage(driver);
+            search.SearchMobile(data.password);       
        }
-   }
+    }
 }
