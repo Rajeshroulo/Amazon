b47330f87b21660a5688f639fc82e9aa0d189717
Rajeshroulo <rajraval017@gmail.com>
2020-10-17 15:38:35 +0530

    Added code to logout from amazon

--- Amazon/Pages/AddressPage.cs
+++ Amazon/Pages/AddressPage.cs
@@ -16,7 +16,7 @@ namespace Amazon.Pages
         [FindsBy(How = How.XPath, Using = "//a[@id='hlb-ptc-btn-native']")]
         public IWebElement proceed;
 
-        [FindsBy(How = How.XPath, Using = "/html/body/div[5]/div[2]/div/div[2]/form[1]/div/div[1]/div[2]/span/a")]
+        [FindsBy(How = How.XPath, Using = "/html/body/div[5]/div[2]/div/div[1]/form[1]/div/div[1]/div[2]/span/a")]
         public IWebElement address;
 
         public void EnterAddress()
---
+++ Amazon/Pages/Logout.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace Amazon.Pages
+{
+    public class Logout
+    {
+        public IWebDriver driver;
+        public Logout(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using = "//span[contains(text(),'Hello, Rajesh')]")]
+        public IWebElement account;
+
+        [FindsBy(How = How.XPath, Using = "//span[contains(text(),'Sign Out')]")]
+        public IWebElement signOut;
+
+      public void AccountLogout()
+      {
+            account.Click();
+            signOut.Click();
+      }
+
+    }
+}
