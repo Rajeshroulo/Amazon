e51653c3dd0b2b7c7777804a57f3ec040f266fe7
Rajeshroulo <rajraval017@gmail.com>
2020-11-06 15:35:34 +0530

    Added feature to bought the product from a seller

--- Amazon/Pages/AddressPage.cs
+++ Amazon/Pages/AddressPage.cs
@@ -13,7 +13,7 @@ namespace Amazon.Pages
             PageFactory.InitElements(driver, this);
         }
 
-        [FindsBy(How = How.XPath, Using = "/html/body/div[1]/div[3]/div/div[1]/div/div/div/div[2]/div[5]/div/form/span/span/span/input")]
+        [FindsBy(How = How.XPath, Using = "//body/div[@id='a-page']/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[5]/div[1]/form[1]/span[1]/span[1]/span[1]/input[1]")]
         public IWebElement cart;
 
         [FindsBy(How = How.XPath, Using = "//a[@id='hlb-ptc-btn-native']")]
